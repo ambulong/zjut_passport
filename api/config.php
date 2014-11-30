3d77b00782f37f9bb19780da96504106c77edbc5
Ambulong <zeng.ambulong@gmail.com>
2014-11-30 21:27:53 +0800

    add base file

---
+++ api/config.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * The name of the MySQL database
+ */
+define ( 'Z_DB_NAME', 'zjutpassport' );
+/**
+ * MySQL database username
+ */
+define ( 'Z_DB_USER', 'zjutpassport' );
+/**
+ * MySQL database password
+ */
+define ( 'Z_DB_PASSWORD', '8QWjemescvDa8HhG' );
+/**
+ * MySQL hostname
+ */
+define ( 'Z_DB_HOST', 'localhost' );
+/**
+ * Database Charset to use in creating database tables.
+ */
+define ( 'Z_DB_CHARSET', 'utf8' );
+/**
+ * The timezone in your area.
+ */
+define ( 'Z_TIMEZONE', 'Asia/Shanghai' );
+/**
+ * For developers
+ */
+define ( 'Z_DEBUG', true );
+/**
+ * Database Table prefix.
+ */
+$table_prefix = 'z_';
