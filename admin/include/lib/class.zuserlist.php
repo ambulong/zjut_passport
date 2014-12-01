5f59453be204172133279aaab57ea331de246b3c
Ambulong <zeng.ambulong@gmail.com>
2014-12-01 19:13:30 +0800

    implement class.zUserList

---
+++ admin/include/lib/class.zuserlist.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * 用户列表
+*
+*/
+class zUserList {
+
+	private $dbh = NULL;
+
+	public function __construct() {
+		$this->dbh = $GLOBALS ['z_dbh'];
+	}
+	
+	public function getList($offset = 0, $rows = 0) {
+		global $table_prefix;
+		$offset = intval($offset);
+		$rows = intval($rows);
+		try {
+			$sth = $this->dbh->prepare ( "SELECT * FROM {$table_prefix}users LIMIT {$offset},{$row}" );
+			$sth->execute ();
+			$result = $sth->fetchAll ( PDO::FETCH_ASSOC );
+			if (count ( $result ) <= 0) {
+				$result = array ();
+			}
+			return $result;
+		}catch ( PDOExecption $e ) {
+			echo "<br>Error: " . $e->getMessage ();
+		}
+	}
+}
