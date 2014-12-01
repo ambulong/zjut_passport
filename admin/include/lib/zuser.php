7c4f7654a3a7054b5502a5e8075a51a412db31ef
Ambulong <zeng.ambulong@gmail.com>
2014-12-01 19:23:49 +0800

    implement class.zUser

---
+++ admin/include/lib/zuser.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * 用户操作
+ * 
+ */
+class zUser {
+	
+	private $dbh = NULL;
+	
+	public function __construct() {
+		$this->dbh = $GLOBALS ['z_dbh'];
+	}
+	
+	public function isExistID($id) {
+		$id = intval ( $id );
+		global $table_prefix;
+		try {
+			$sth = $this->dbh->prepare ( "SELECT count(*) FROM {$table_prefix}users WHERE `id` = :id " );
+			$sth->bindParam ( ':id', $id );
+			$sth->execute ();
+			$row = $sth->fetch ();
+			if ($row [0] > 0) {
+				return TRUE;
+			} else {
+				return FALSE;
+			}
+		} catch ( PDOExecption $e ) {
+			echo "<br>Error: " . $e->getMessage ();
+		}
+	}
+	
+	public function getDetail($id) {
+		global $table_prefix;
+		$id = intval ( $id );
+		if (! $this->isExistID ( $id )) {
+			return FALSE;
+		}
+		try {
+			$sth = $this->dbh->prepare ( "SELECT * FROM {$table_prefix}users WHERE `id` = :id " );
+			$sth->bindParam ( ':id', $value );
+			$sth->execute ();
+			$result = $sth->fetch ( PDO::FETCH_ASSOC );
+			return $result;
+		} catch ( PDOExecption $e ) {
+			echo "<br>Error: " . $e->getMessage ();
+		}
+	}
+	
+	public function getMoreDetail($id) {
+		global $table_prefix;
+		$id = intval ( $id );
+		if (! $this->isExistID ( $id )) {
+			return FALSE;
+		}
+		try {
+			$sth = $this->dbh->prepare ( "SELECT * FROM {$table_prefix}users_detail WHERE `uid` = :id " );
+			$sth->bindParam ( ':id', $value );
+			$sth->execute ();
+			$result = $sth->fetch ( PDO::FETCH_ASSOC );
+			return $result;
+		} catch ( PDOExecption $e ) {
+			echo "<br>Error: " . $e->getMessage ();
+		}
+	}
+	
+}
